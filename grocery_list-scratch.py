db95448a515b85817bb1dc48a4dc80c858025885
Lumos393 <64517124+Lumos393@users.noreply.github.com>
2020-09-14 15:30:01 -0400

    Update grocery_list-scratch.py

--- grocery_list-scratch.py
+++ grocery_list-scratch.py
@@ -1,4 +1,26 @@
 import tkinter as tk
 
 window = tk.Tk()
-label = tk.Label(text="\nThank you for using Sam's Grocery Thingy!\n")
+choice_a = tk.Frame()
+choice_i = tk.Frame()
+
+button_a = tk.Button(
+    text="Alexa",
+    width=10,
+    height=5,
+    master=choice_a
+)
+button_a.pack()
+
+button_i = tk.Button(
+    text="Individual",
+    width=10,
+    height=5,
+    master=choice_i
+)
+button_i.pack()
+
+choice_a.pack()
+choice_i.pack()
+
+window.mainloop()
